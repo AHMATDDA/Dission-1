97def447fb4bfd6b3fef3b3a2ab928b0e2985c26
coals9230 <studycoals329@gmail.com>
2022-05-06 15:01:17 +0900

    test

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //  push 테스트 파일입니다
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
